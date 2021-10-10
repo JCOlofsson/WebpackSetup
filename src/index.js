5471e14ef356253a9e516fbc30b1c30c3e438089
Christofer Olofsson <christofer.j.olofsson@gmail.com>
2021-10-10 21:03:35 +0200

    Initial setup step 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("js file is working")
+import $ from 'jquery'
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = "Hello OH!"
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+  $(document).append('<p>jQuery loaded!</p>')
