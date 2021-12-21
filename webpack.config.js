52601c3832169ac32e749924a8441a5d1a3c972a
Christofer Olofsson <christofer.j.olofsson@gmail.com>
2021-12-21 23:54:28 +0100

    Extract style to separate css files (minimized and prefixed).

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,71 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  // entry: './src/index.js',
+  entry: {
+    main: [
+      path.resolve(__dirname, './src/index.js'), 
+      path.resolve(__dirname, './src/style.sass')
+    ],
   },
-  module:{
+  output: {
+    filename: "main.min.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
     rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            // Loas SCSS, SASS and CSS files,
-            test: /\.s[ac]ss$/,
-            use: [
-                {
-                    // inject CSS to page ('style-loader'), 
-                    loader: 'style-loader'
+      },
+      {
+        // Loas SCSS, SASS and CSS files,
+        test: /\.(sass|scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+              modules: false,
+            },
+          },
+          {
+            // Run postcss actions
+            loader: "postcss-loader",
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require("autoprefixer")];
                 },
-                {
-                    // translates CSS into CommonJS modules ('css-loader'), 
-                    loader: 'css-loader'
-                },
-                {
-                    // Run postcss actions
-                    loader: 'postcss-loader',
-                    options: {
-                    // `postcssOptions` is needed for postcss 8.x;
-                    // if you use postcss 7.x skip the key
-                    postcssOptions: {
-                        // postcss plugins, can be exported to postcss.config.js
-                        plugins: function () {
-                            return [
-                                require('autoprefixer')
-                            ];
-                        }
-                    }
-                    }
-                },
-                {
-                    // compiles Sass to CSS
-                    loader: 'sass-loader'
-                }
-            ]
-        }
-    ]
-  }
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: "style.min.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
